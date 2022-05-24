08d625aa9703320a86a7a9ed14bd001085bf1390
Pretz333 <wpretzel@hotmail.com>
2022-05-23 21:49:59 -0500

    Problem #32 Write-Up
    Also a small cleanup by changing the character counter else if to simply an else, since there are only two options. I didn't re-run it to see the times since it's more for readability than code efficiency.

--- problems/32-Longest-Valid-Parentheses/solution.cs
+++ problems/32-Longest-Valid-Parentheses/solution.cs
@@ -1,19 +1,27 @@
 public class Solution {
     public int LongestValidParentheses(string s) {
+        // The result, the length of the substring
         int count = 0;
-        
+
+        // These will count the number of '(' and ')'
         int left = 0;
         int right = 0;
         
-        for (int i = 0; i < s.Length; i++) {
+        // The first pass will check characters going from
+        // left to right along the provided string.
+        for (int i = 0; i < s.Length; i++) { // O(n)
+            // Add 1 based on if it's a left or a right
             if (s[i] == '(') {
                 left++;
-            }
-            
-            if (s[i] == ')') {
+            } else {
                 right++;
             }
             
+            // If we have an equal amount of left and rights, we have a valid substring. 
+            // Update count to be whichever is higher, the longest valid substring up to this
+            // point (the current count) or the current valid substring (left + right)
+            // If we have more rights than lefts, reset our valid substring counts. This 
+            // will reset on substrings like "))" (we'll get the other half on the next for loop)
             if (left == right) {
                 count = Math.Max(count, left + right);
             } else if (right > left) {
@@ -22,18 +30,25 @@ public class Solution {
             }
         }
         
+        // Reset the counters
         left = 0;
         right = 0;
         
-        for (int i = s.Length - 1; i >= 0; i--) {
+        // The second pass will check characters going from
+        // right to left along the provided string.
+        for (int i = s.Length - 1; i >= 0; i--) { // O(n)
+            // Add 1 based on if it's a left or a right
             if (s[i] == '(') {
                 left++;
-            }
-            
-            if (s[i] == ')') {
+            } else {
                 right++;
             }
             
+            // If we have an equal amount of left and rights, we have a valid substring. 
+            // Update count to be whichever is higher, the longest valid substring up to this
+            // point (the current count) or the current valid substring (left + right)
+            // If we have more lefts than rights, reset our valid substring counts. This 
+            // will reset on substrings like "(("
             if (left == right) {
                 count = Math.Max(count, left + right);
             } else if (left > right) {
@@ -41,6 +56,9 @@ public class Solution {
             }
         }
         
+        // Return our final result
         return count;
+        
+        // Since the for loops are next to each other (not one inside of the other), our solution is O(n + n), which is simply O(n).
     }
 }
